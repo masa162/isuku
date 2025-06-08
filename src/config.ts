2f4ddd153b178b32da869eb65bf5c062b96d56a3
masa162 <belong2jazz@gmail.com>
2025-06-08 12:34:50 +0900

    rv

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = "医スク！ | 医学論文 × 解説ポータル";
+export const SITE_DESCRIPTION = '医スク！ | 医学論文 × 解説ポータル';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
